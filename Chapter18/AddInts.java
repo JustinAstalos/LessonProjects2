8a203043e402346bbb7d8549e9af4228bc031978
JustinAstalos <astalosjt@s.dcsdk12.org>
2018-09-26 08:30:25 -0600

    Do the lesson projects

---
+++ Chapter18/AddInts.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Adder
+{  
+  public static void main ( String[] args )  
+  {
+    int x;
+    Scanner scan = new Scanner( System.in );
+    System.out.println("Enter an integer (Enter 0 to quit):");
+    x = scan.nextInt();
+    int y = 0;
+    int total = 0;
+    if (x == 0)
+    {
+        System.out.println("bye");
+    }
+    else
+    {
+        System.out.println("Enter another integer:");
+        y = scan.nextInt();
+        total = x+y;
+        System.out.println("The total is "+total);
+    }
+  }
+}
